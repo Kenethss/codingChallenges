5f39983cacd464b140ec789b781f586c047d2a0c
Kenny S <kennethstrunk@yahoo.com>
2025-01-22 18:30:10 -0700

    array practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -1098,4 +1098,24 @@
 //        res.push(arr[i])
 //    }
 //    return res
-//  }
+// //  }
+// The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+// Task:
+
+// Given an array of Player objects and a position (first position is 1), return the name of the chosen Player.
+// name is a property of Player objects, e.g Player.name
+
+// Example:
+
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+
+// function duckDuckGoose(players, goose) {
+//   for(let i = 0; i < players.length; i++){
+//     if (i === (goose - 1) % players.length){
+//       return players[i].name
+//     }
+//   }
+// }
