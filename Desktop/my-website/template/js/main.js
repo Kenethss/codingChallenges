c47135a1bdbab8d900ee3680697419e25c11083a
Kenny S <kennethstrunk@yahoo.com>
2025-02-07 18:44:13 -0700

    array practice

--- Desktop/my-website/template/js/main.js
+++ Desktop/my-website/template/js/main.js
@@ -1279,3 +1279,14 @@
 
 //     return found
 
+// Let's add another function to our inventory system. Write a function that returns the first element from a list. If the list is empty then return the string ERROR instead.
+
+// def get_first_item(items):
+//     long = len(items)
+//     if long == 0:
+//         return "ERROR"
+//     else:
+//         return items[0]
+        
+
+    
